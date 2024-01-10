5d012d100cc33d21e9468c98cfa3ccca0705be4b
praveen <59041973+praveen9177496079@users.noreply.github.com>
2024-01-10 20:11:13 +0530

    Update s3_backend.tf

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terra-backend-s3"
-    key    = "db-workspace"
-    region = "us-east-2"
+    bucket = "dbr-s3-bucket"
+    key    = "db-ws-state"
+    region = "us-east-1"
   }
 }
